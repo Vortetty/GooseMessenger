196eaddd047fb3956f5a83c8606241dbe04ff9b4
Vortetty <33466216+Vortetty@users.noreply.github.com>
2020-06-05 13:30:13 -0700

    Add files via upload

--- GooseMod_Goosenger - public/DefaultMod/ModMain.cs
+++ GooseMod_Goosenger - public/DefaultMod/ModMain.cs
@@ -5,13 +5,18 @@ using GooseShared;
 using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Web.Script.Serialization;
 using System.Windows.Forms;
 using System.Xml;
+using Hjson;
+using ProfanityFilter;
 
 namespace GooseMessaging
 {
@@ -21,6 +26,36 @@ namespace GooseMessaging
         // useful functions we need to interface with the goose.
         void IMod.Init()
         {
+            //config stuff
+            string configpath = Path.Combine(API.Helper.getModDirectory(this), "config.hjson");
+
+            if (!File.Exists(configpath))
+            {
+                using (StreamWriter sw = File.CreateText(configpath))
+                {
+                    sw.Write("");
+                    sw.Close();
+                }
+                var jsonObject = new JsonObject
+                {
+                  { "profanitiesAllowed", false }
+                };
+                HjsonValue.Save(jsonObject, configpath);
+            }
+
+            //if (!config.Item1)
+            //{
+            //    genSwears();
+            //}
+            //if (!config.Item2)
+            //{
+            //    genSexuals();
+            //}
+            //if (!config.Item3)
+            //{
+            //    genOther();
+            //}
+
             // Subscribe to whatever events we want
             InjectionPoints.PostTickEvent += PostTick;
         }
@@ -29,6 +64,58 @@ namespace GooseMessaging
         public static extern short GetAsyncKeyState(Keys vKey);
         bool failsafe = false;
         string input;
+        //List<List<string>> swearList = new List<List<string>>();
+
+        public bool loadConfig()
+        {
+            string configpath = Path.Combine(API.Helper.getModDirectory(this), "config.hjson");
+            var jsonObject = HjsonValue.Load(configpath).Qo();
+            return jsonObject.Qb("profanitiesAllowed");
+        }
+
+        /*public void genSwears()
+        {
+            swearList.Add(new List<string> { "ass", "a**" });
+            swearList.Add(new List<string> { "bastard", "b******" });
+            swearList.Add(new List<string> { "bitch", "b****" });
+            swearList.Add(new List<string> { "cunt", "c***" });
+            swearList.Add(new List<string> { "fuck", "f***" });
+            swearList.Add(new List<string> { "shit", "s***" });
+            swearList.Add(new List<string> { "slut", "sl**" });
+            swearList.Add(new List<string> { "whore", "w***e" });
+            swearList.Add(new List<string> { "twat", "t**t" });
+            swearList.Add(new List<string> { "shag", "passionately hug" });
+            swearList.Add(new List<string> { "sex", "passionately hug" });
+        }
+
+        public void genSexuals()
+        {
+            swearList.Add(new List<string> { "anal", "a**l" });
+            swearList.Add(new List<string> { "alabama hot pocket", "alabama hot pocket (dont look it up)" });
+            swearList.Add(new List<string> { "penis", "p***s" });
+            swearList.Add(new List<string> { "vagina", "v****a" });
+            swearList.Add(new List<string> { "erotic", "s**ual" });
+            swearList.Add(new List<string> { "twat", "t**t" });
+            swearList.Add(new List<string> { "dick", "d**k" });
+            swearList.Add(new List<string> { "pussy", "p***y" });
+            swearList.Add(new List<string> { "tit", "t*t" });
+            swearList.Add(new List<string> { "tiddy", "t***y" });
+            swearList.Add(new List<string> { "knockers", "breasts" });
+            swearList.Add(new List<string> { "boobs", "breasts" });
+            swearList.Add(new List<string> { "tiddy", "t***y" });
+            swearList.Add(new List<string> { "blowjob", "t***y" });
+            swearList.Add(new List<string> { "blow job", "t***y" });
+            swearList.Add(new List<string> { "bondage", "b*****e" });
+            swearList.Add(new List<string> { "boner", "b***r" });
+            swearList.Add(new List<string> { "buttplug", "butt p**g" });
+            swearList.Add(new List<string> { "cameltoe", "c******e" });
+            swearList.Add(new List<string> { "clit", "cl*t" });
+            swearList.Add(new List<string> { "dildo", "d***o" });
+        }
+
+        public void genOther()
+        {
+        }*/
 
         public static bool haswifi()
         {
@@ -42,6 +129,37 @@ namespace GooseMessaging
             {
                 return false;
             }
+        }
+
+        public static String stripNonUTF8(string inputValue)
+        {
+            return Regex.Replace(inputValue, @"[^\u0000-\u00FF]", String.Empty);
+        }
+
+        static string stripDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        static string replaceSwears(string text, List<List<string>> swears)
+        {
+            foreach (List<string> replaceVal in swears)
+            {
+                Regex.Replace(text, replaceVal[0], replaceVal[1], RegexOptions.IgnoreCase);
+            }
+            return text;
         }
 
         public void PostTick(GooseEntity g)
@@ -63,7 +181,7 @@ namespace GooseMessaging
                         if (!String.IsNullOrWhiteSpace(input))
                         {
                             XmlDocument document = new XmlDocument();
-                            document.Load("Url here" + System.Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(input)));
+                            document.Load("url here" + System.Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(input)));
                             string webData = document.InnerText;
                             if (webData == "saved")
                             {
@@ -73,9 +191,14 @@ namespace GooseMessaging
                                 if (dialogResult == DialogResult.Yes)
                                 {
                                     XmlDocument document1 = new XmlDocument();
-                                    document1.Load("Url Here");
-                                    string webData1 = document1.InnerText;
-                                    MessageBox.Show(System.Text.ASCIIEncoding.ASCII.GetString(System.Convert.FromBase64String(webData1)), "Message From Other Geese");
+                                    document1.Load("url here");
+                                    string webData1 = System.Text.ASCIIEncoding.ASCII.GetString(System.Convert.FromBase64String(document1.InnerText));
+                                    if (loadConfig() != true)
+                                    {
+                                        var filter = new ProfanityFilter.ProfanityFilter();
+                                        webData1 = filter.CensorString(webData1);
+                                    }
+                                    MessageBox.Show(webData1, "Message From Other Geese");
                                 }
                             }
                         }
---
+++ GooseMod_Goosenger - public/DefaultMod/WhiteList.cs
@@ -0,0 +1,116 @@
+﻿/*
+MIT License
+Copyright (c) 2019 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using ProfanityFilter.Interfaces;
+
+namespace ProfanityFilter
+{
+    public class WhiteList : IWhiteList
+    {
+        List<string> _whiteList;
+
+        public WhiteList()
+        {
+            _whiteList = new List<string>();
+        }
+
+        /// <summary>
+        /// Return an instance of a read only collection containing whitelist
+        /// </summary>
+        public ReadOnlyCollection<string> ToList
+        {
+            get
+            {
+                return new ReadOnlyCollection<string>(_whiteList);
+            }
+        }
+
+        /// <summary>
+        /// Add a word to the profanity whitelist. This means a word that is in the whitelist
+        /// can be ignored. All words are treated as case insensitive.
+        /// </summary>
+        /// <param name="wordToWhitelist">The word that you want to whitelist.</param>
+        public void Add(string wordToWhitelist)
+        {
+            if (string.IsNullOrEmpty(wordToWhitelist))
+            {
+                throw new ArgumentNullException(nameof(wordToWhitelist));
+            }
+
+            if (!_whiteList.Contains(wordToWhitelist.ToLower(CultureInfo.InvariantCulture)))
+            {
+                _whiteList.Add(wordToWhitelist.ToLower(CultureInfo.InvariantCulture));
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="wordToCheck"></param>
+        /// <returns></returns>
+        public bool Contains(string wordToCheck)
+        {
+            if (string.IsNullOrEmpty(wordToCheck))
+            {
+                throw new ArgumentNullException(nameof(wordToCheck));
+            }
+
+            return _whiteList.Contains(wordToCheck.ToLower(CultureInfo.InvariantCulture));
+        }
+
+        /// <summary>
+        /// Return the number of items in the whitelist.
+        /// </summary>
+        /// <returns>The number of items in the whitelist.</returns>
+        public int Count
+        {
+            get
+            {
+                return _whiteList.Count;
+            }
+        }
+
+        /// <summary>
+        /// Remove all words from the whitelist.
+        /// </summary>  
+        public void Clear()
+        {
+            _whiteList.Clear();
+        }
+
+        /// <summary>
+        /// Remove a word from the profanity whitelist. All words are treated as case insensitive.
+        /// </summary>
+        /// <param name="wordToRemove">The word that you want to use</param>
+        /// <returns>True if the word is successfuly removes, False otherwise.</returns>
+        public bool Remove(string wordToRemove)
+        {
+            if (string.IsNullOrEmpty(wordToRemove))
+            {
+                throw new ArgumentNullException(nameof(wordToRemove));
+            }
+
+            return _whiteList.Remove(wordToRemove.ToLower(CultureInfo.InvariantCulture));
+        }
+    }
+}
